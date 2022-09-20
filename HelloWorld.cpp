6fc42887cab3555378bf9f716d7917c460fc2b46
Jacob Powell <jpow401@gmail.com>
2022-09-20 09:52:00 -0700

    First Commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+
+    cout << "Please enter two numbers seperated by a space" << endl;
+    cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    cout << num1 << " + " << num2 << " = "<< sum << endl;
+    return 0;
+}
